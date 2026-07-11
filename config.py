0d3597054c65222d3cd54dc5ad0a69e0654f0acf
hema951code <22f1000951@ds.study.iitm.ac.in>
2026-07-11 09:17:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # ---- FILL THESE TWO IN ----
-EMAIL = "your_email@ds.study.iitm.ac.in"       # your IITM email used to seed the assignment
-AIPIPE_TOKEN = "PASTE_YOUR_AIPIPE_TOKEN_HERE"  # from https://aipipe.org/login
+EMAIL = "22f1000951@ds.study.iitm.ac.in"       # your IITM email used to seed the assignment
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIyZjEwMDA5NTFAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4MDgwNDA1MywiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4MTQwODg1M30.mf1c14lKg6fD55echLwaYBzrcs1erc3pMgdufb8Gv2M"  # from https://aipipe.org/login
 
 # ---- Leave these as-is ----
 AIPIPE_BASE = "https://aipipe.org/openai/v1"
